f04cdacff788a14d76825a01067493cd9b69e56a
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-11-05 15:39:28 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -6,12 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="meals">
+      <h1 style={ { color: 'white' } }>MAIN GROUP 3</h1>
       <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
+      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
         Glass
       </object>
     </div>
