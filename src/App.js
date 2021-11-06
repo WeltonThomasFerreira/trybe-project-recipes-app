688b76179f520c8af119044aa0834e895899c962
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-11-05 22:51:30 -0300

    Teste do avaliador do github

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="meals">
-      <h1 style={ { color: 'white' } }>MAIN GROUP 3</h1>
       <span className="logo">TRYBE</span>
       <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
         Glass
