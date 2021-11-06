1341b790568fd64add8159e07b7be2df1bea72b7
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-11-06 00:26:54 -0300

    Implementa tela de login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import Login from '../components/Login';
 
 export default function Home() {
-  return <>Home</>;
+  return (
+    <>
+      <h1>Home</h1>
+      <Login />
+    </>
+  );
 }
