a0aa26e062b4231af7363bf0e5060f59b5061182
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-11-06 03:20:07 -0300

    Req 13. Implementa os elementos da barra de busca respeitando os atributos descritos no protótipo

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,13 +8,39 @@ const container = {
 
 export default function SearchBar() {
   return (
-    <div style={ container }>
-      <input
-        data-testid="search-input"
-        type="text"
-        placeholder="Buscar receita"
-      />
-      <button type="button">Buscar</button>
-    </div>
+    <>
+      <div style={ container }>
+        <fieldset>
+          <input
+            data-testid="search-input"
+            type="text"
+            placeholder="Buscar receita"
+          />
+          <button data-testid="exec-search-btn" type="button">
+            Buscar
+          </button>
+        </fieldset>
+      </div>
+      <div style={ container }>
+        <fieldset>
+          <label htmlFor="ingredient">
+            <input
+              data-testid="ingredient-search-radio"
+              type="radio"
+              id="ingredient"
+            />
+            Ingrediente
+          </label>
+          <label data-testid="name-search-radio" htmlFor="name">
+            <input type="radio" id="name" />
+            Nome
+          </label>
+          <label data-testid="first-letter-search-radio" htmlFor="firstLetter">
+            <input type="radio" id="firstLetter" />
+            Primeira Letra
+          </label>
+        </fieldset>
+      </div>
+    </>
   );
 }
