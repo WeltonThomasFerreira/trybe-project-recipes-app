fb5a32dc5f43beb10c0359644c4d6b58aaf26eb7
mfelipperd <felipperabelodurans@gmail.com>
2021-11-08 20:38:37 -0300

    implementa Explorar Comidas

--- src/pages/ExploreFood.jsx
+++ src/pages/ExploreFood.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -8,6 +9,31 @@ export default function ExploreFood() {
   return (
     <>
       <Header title={ title } />
+      <Link to="/explorar/comidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+
+        </button>
+      </Link>
+      <Link to="/explorar/comidas/area">
+        <button
+          type="button"
+          data-testid="explore-by-area"
+        >
+          Por Local de Origem
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
+      </Link>
       ;
       <Footer />
     </>
