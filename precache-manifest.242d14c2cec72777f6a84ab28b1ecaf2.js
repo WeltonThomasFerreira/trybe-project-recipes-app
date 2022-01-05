412badabac21b44e96d587551a5bee3d85dfa6b2
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-01-04 21:53:36 -0300

    Updates

---
+++ precache-manifest.242d14c2cec72777f6a84ab28b1ecaf2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf711d0c68369b8f34e11a9f9a8c5d7",
+    "url": "/trybe-project-recipes-app/index.html"
+  },
+  {
+    "revision": "17726e1212c97f5666eb",
+    "url": "/trybe-project-recipes-app/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "8867c60501bde2d72eee",
+    "url": "/trybe-project-recipes-app/static/css/main.f82d62c9.chunk.css"
+  },
+  {
+    "revision": "17726e1212c97f5666eb",
+    "url": "/trybe-project-recipes-app/static/js/2.2635ae5d.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/trybe-project-recipes-app/static/js/2.2635ae5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8867c60501bde2d72eee",
+    "url": "/trybe-project-recipes-app/static/js/main.fff152b1.chunk.js"
+  },
+  {
+    "revision": "a2f3cbe53e94e9c4f1cc",
+    "url": "/trybe-project-recipes-app/static/js/runtime-main.30ba38e2.js"
+  },
+  {
+    "revision": "d61725e17eb06d975735c6b89b8ac6de",
+    "url": "/trybe-project-recipes-app/static/media/chiquem.d61725e1.png"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/trybe-project-recipes-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/trybe-project-recipes-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/trybe-project-recipes-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/trybe-project-recipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/trybe-project-recipes-app/static/media/searchIcon.db812135.svg"
+  }
+]);
