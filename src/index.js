4fdb6bb53ae19bd635142c236ac2434321eff7b6
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-01-04 21:51:31 -0300

    Muda router para hash

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
@@ -8,7 +8,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Router>
+  <Router basename="/trybe-project-recipes-app">
     <Provider store={ store }>
       <App />
     </Provider>
